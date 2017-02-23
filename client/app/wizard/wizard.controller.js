87a5f9c7006bde5711d1851e8e5d2272822156cc
Uice Lu <uicestone@gmail.com>
2017-02-23 09:06:13 +0800

    关键节点页渠道时间段

--- client/app/wizard/wizard.controller.js
+++ client/app/wizard/wizard.controller.js
@@ -27,18 +27,28 @@
             {name:'四个辣妈', topic:'母婴', rank:3}
         ];
 
-        $scope.project = {};
+        $scope.project = {startDate:'2016-11-11', endDate:'2016-11-30'};
 
-        $scope.project.kpis = [{}];
+        $scope.project.kpis = [{type:'提交次数',value:8888}, {type:'分享次数', value:3000}];
+        $scope.project.channels = [
+            {name:'同道大叔', startDate:'2016-11-13', endDate:'2016-11-22'},
+            {name:'姜茶茶', startDate:'2016-11-14', endDate:'2016-11-25'},
+        ];
 
         $scope.addKpiForm = function() {
             $scope.project.kpis.push({});
         };
+        $scope.project.duration = new Date($scope.project.endDate) - new Date($scope.project.startDate) + 86400000;
+        $scope.project.channels.map(channel => {
+            channel.startDatePercentage = (new Date(channel.startDate) - new Date($scope.project.startDate)) / $scope.project.duration * 100;
+            channel.endDatePercentage = (new Date(channel.endDate) - new Date($scope.project.startDate)) / $scope.project.duration * 100;
+            channel.durationPercentage = (new Date(channel.endDate) - new Date(channel.startDate)) / $scope.project.duration * 100;
+            return channel;
+        });
 
-        $scope.$watch('channels', function(channels) {
-            console.log(channels);
-            $scope.project.channels = channels.filter(channel => channel.selected);
-        }, true);
+        // $scope.$watch('channels', function(channels) {
+        //     $scope.project.channels = channels.filter(channel => channel.selected);
+        // }, true);
 
     }
     
