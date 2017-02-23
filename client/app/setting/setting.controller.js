cccd18e76496232a862be0dd05177a25abdb2ae1
Uice Lu <uicestone@gmail.com>
2017-02-23 15:27:48 +0800

    渠道管理编辑初步

--- client/app/setting/setting.controller.js
+++ client/app/setting/setting.controller.js
@@ -2,9 +2,9 @@
     'use strict';
 
     angular.module('app.setting')
-    .controller('settingCtrl', ['$scope', '$window', settingCtrl]);
+    .controller('settingCtrl', ['$scope', '$window', '$mdBottomSheet', '$mdToast', settingCtrl]);
 
-    function settingCtrl($scope, $window) {
+    function settingCtrl($scope, $window, $mdBottomSheet, $mdToast) {
         $scope.users = [
             {name: 'Tracy'},
             {name: 'David'},
@@ -26,6 +26,22 @@
             {name:'雌时尚', topic:'时尚', rank:5},
             {name:'四个辣妈', topic:'母婴', rank:3}
         ];
+
+        $scope.channelTopics = ['星座','娱乐','时尚','母婴'];
+
+        $scope.editChannel = function(channel) {
+            $scope.alert = '';
+            $scope.channel = channel;
+            $mdBottomSheet.show({
+                templateUrl: 'app/setting/channel-bottom-sheet.html',
+                scope: $scope,
+                clickOutsideToClose: false
+            });
+        };
+
+        $scope.updateChannel = function(channel) {
+            $mdBottomSheet.hide();
+        };
 
     }
     
