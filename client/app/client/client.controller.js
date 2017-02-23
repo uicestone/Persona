c0220530d236fc47723ac52aeef7801d2c8648af
Uice Lu <uicestone@gmail.com>
2017-02-23 16:18:59 +0800

    用户画像列表初步

--- client/app/client/client.controller.js
+++ client/app/client/client.controller.js
@@ -5,7 +5,19 @@
     .controller('clientCtrl', ['$scope', '$window', clientCtrl]);
 
     function clientCtrl($scope, $window) {
-        
+        $scope.query = {withTags:[], withoutTags:[]};
+        $scope.showKeys = [
+            {key:'mobile', label:'手机号码', show:true},
+            {key:'name', 'label':'姓名', show:true},
+            {key:'region', 'label':'地区', show:true},
+            {key:'sex', 'label':'性别', show:true},
+            {key:'tags', 'label':'标签', show:true}
+        ];
+        $scope.otherKeys = [
+            {key:'education', label:'学历'},
+            {key:'age', label:'年龄'},
+            {key:'marriage', label:'婚否'}
+        ];
     }
     
 })(); 
