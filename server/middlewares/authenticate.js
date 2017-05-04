ef1a8204dd5e6cdd406e20ee50baef207a6788cf
Uice Lu <uicestone@gmail.com>
2017-05-05 00:54:49 +0800

    允许微信授权API无登录使用

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -2,30 +2,25 @@ const User = require('../models/user');
 
 module.exports = function(req, res, next) {
 
-    if(req.originalUrl === '/api/auth/login') {
-        next();
-        return;
-    }
-
-    if(req.originalUrl.match(/^\/api\/wechat\-auth/)) {
-        next();
-        return;
-    }
+    const strictAuth = !['/api/auth/login', '/api/wechat-auth'].some(path => req.originalUrl.match(new RegExp('^' + path)));
 
     const token = req.get('authorization') || req.query.token;
 
-    if(!token) {
+    if(!token && strictAuth) {
         res.status(401).json({message:'无效登录，请重新登录'});
         return;
     }
 
     User.findOne({token}).then((user) => {
-        if(!user) {
+        if(!user && strictAuth) {
             res.status(401).json({message:'无效登录，请重新登录'});
             return;
         }
 
-        req.user = user;
+        if (user) {
+            req.user = user;
+        }
+
         next();
     });
 
