eedb9e1e19cc6e7f2b6115ca8bf32ae29a1b2c09
Uice Lu <uicestone@gmail.com>
2017-11-01 14:27:32 +0800

    fix: 高级搜索可能匹配错误

--- server/apis/customer.js
+++ server/apis/customer.js
@@ -75,7 +75,7 @@ module.exports = (router) => {
                         [key]: {$exists: false}
                     });
                 }
-                else if (value.indexOf && value.indexOf(',') > -1) {
+                else if (typeof value === 'string' && value.indexOf(',') > -1) {
                     query.find({
                         [key]: {$in: value.split(',')}
                     })
