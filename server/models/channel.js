a8e28f22cf40c92e514407ae98603a2a2ce464f7
Uice Lu <uicestone@gmail.com>
2017-12-25 14:23:13 +0800

    渠道字段整理

--- server/models/channel.js
+++ server/models/channel.js
@@ -3,20 +3,18 @@ const Schema = mongoose.Schema;
 
 const channelSchema = new Schema({
     spid: String,
-    uuid: String,
     name: String,
     platform: String,
-    wechatId: String,
     mcnId: String,
     topic: String,
     tags: [String],
     fans: Number,
     rank: Number,
     distributionAbility: Number,
-    score: Number,
-    wasIn500: Boolean,
     updatedAt: Date,
-    remark: String
+    remark: String,
+    identified: Boolean
+
 });
 
 channelSchema.index({spid:1}, {unique:true, sparse:true});
